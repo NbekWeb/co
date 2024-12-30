897fd472d1b5604cfb49e52d8ad8ec695da2527d
NbekWeb <nbekdev@gmail.com>
2024-12-30 23:05:10 +0500

    first commit

--- src/components/OurWork.vue
+++ src/components/OurWork.vue
@@ -25,16 +25,33 @@ import Works from "./Works.vue";
       <div
         class="grid items-center grid-cols-3 gap-10 py-10 max-lg:grid-cols-1"
       >
-        <video
-          src="@/assets/img/gi.mp4"
-          class="col-span-2 max-lg:col-span-1 object-cover w-full h-full "
-          autoplay
-          loop
-          muted
-        ></video>
-        <div class="flex flex-col items-center text-white/50 gap-7">
-          <img src="@/assets/img/logo.png" class="h-24" />
-          <p class="2xl:text-2xl xl:text-base">
+        <div class="relative col-span-2 p-10 max-lg:p-4">
+          <img
+            src="@/assets/img/r1.png"
+            class="w-[280px] absolute top-0 right-0 max-lg:w-[180px]"
+          />
+          <img
+            src="@/assets/img/r2.png"
+            class="w-[280px] absolute bottom-0 left-0 max-lg:w-[180px]"
+          />
+
+          <video
+            src="@/assets/img/gi.mp4"
+            class="object-cover w-full h-full max-lg:col-span-1"
+            autoplay
+            loop
+            muted
+          ></video>
+        </div>
+        <div
+          class="relative flex flex-col items-center h-full text-white/50 gap-7"
+        >
+          <img
+            src="@/assets/img/lbg.png"
+            class="opacity-[8%] h-full absolute top-0 right-0"
+          />
+          <img src="@/assets/img/logo.png" class="h-24 relative z-[10]" />
+          <p class="2xl:text-2xl xl:text-base relative z-[10]">
             {{ $t("text_1") }}
           </p>
         </div>
