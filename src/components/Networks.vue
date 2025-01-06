8e7ea976d35e58a4f39ef9fbaf274332c64729d1
NbekWeb <nbekdev@gmail.com>
2025-01-06 23:24:59 +0500

    first commit

--- src/components/Networks.vue
+++ src/components/Networks.vue
@@ -7,7 +7,7 @@ import ve from "./icons/ve.vue";
 <template>
   <div class="flex flex-col items-center gap-4 max-md:flex-row max-md:gap-1.5">
     <a
-      class="flex w-[74px] h-[74px] items-center group justify-center icon group p-2 "
+      class="flex w-[74px] h-[74px] items-center group justify-center icon group p-2 hover:scale-125"
     >
       <tg class="hidden mt-1 mr-2 text-2xl group-hover:flex" />
       <img
@@ -16,13 +16,13 @@ import ve from "./icons/ve.vue";
       />
     </a>
     <a
-      class="flex w-[74px] h-[74px] items-center justify-center icon group p-2"
+      class="flex w-[74px] h-[74px] items-center justify-center icon group p-2 hover:scale-125"
     >
       <be class="hidden text-2xl group-hover:flex" />
       <img src="@/assets/img/dis.png" class="w-full group-hover:hidden" />
     </a>
     <a
-      class="flex w-[74px] h-[74px] items-center justify-center icon group p-2"
+      class="flex w-[74px] h-[74px] items-center justify-center icon group p-2 hover:scale-125"
     >
       <ve class="hidden text-2xl group-hover:flex" />
       <img src="@/assets/img/ve.png" class="w-full group-hover:hidden" />
